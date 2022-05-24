ed4e2b44d3452181531e90b93c69be1375c7a581
SohanaN <sohananaz01@gmail.com>
2022-05-25 02:10:41 +0600

    Footer  added

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,10 +1,50 @@
 import React from 'react';
-
+import { Col, Container, Nav, Row } from 'react-bootstrap';
+import footer from '../../assets/images/footer-pic.jpg'
+import CustomLink from './CustomLink';
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            <h1>footer</h1>
-        </div>
+        <footer style={{
+            background: `url(${footer})`,
+            backgroundSize: 'cover'
+        }} className="text-white">
+            <div className="p-5">
+                <Container>
+                    <Row>
+                        <Col xs={3} md={3}>
+                            <h2>Links</h2>
+                            <Nav className="ms-auto gap-4 ">
+                                <CustomLink to='/'>Home</CustomLink>
+                                <CustomLink to='/parts'>Parts</CustomLink>
+                                <CustomLink to='/portfolio'>My Portfolio</CustomLink>
+                                <CustomLink to='/blogs'>Blogs</CustomLink>
+                                <CustomLink to='/about'>About Us</CustomLink>
+                            </Nav>
+                        </Col>
+                        <Col xs={6} md={6}>
+                            <div className="d-flex justify-content-center">
+                                <div>
+                                    <h2>SN Car Parts Co.</h2>
+                                    <small>copyright &copy; {year} All rights reserved.</small>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={3} md={3}>
+                            <div className="d-flex justify-content-end">
+                                <Nav className="ms-auto gap-4 ">
+                                    <CustomLink to=' '>Legal</CustomLink>
+                                    <CustomLink to=' '>Terms of use</CustomLink>
+                                    <CustomLink to=' '>Privacy policy</CustomLink>
+                                    <CustomLink to=' '>Cookie policy</CustomLink>
+                                </Nav>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </footer>
     );
 };
 
