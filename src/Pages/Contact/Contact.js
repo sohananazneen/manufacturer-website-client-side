aa548ab96d8375776ac59cfd638505459e41d285
SohanaN <sohananaz01@gmail.com>
2022-05-26 01:32:55 +0600

    Contact added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className='my-4'>
+            <h2 className='text-center'>Contact Us</h2>
+            <div className='d-flex justify-content-center'>
+                <Form className='w-50 border border-danger text-center p-5 shadow-lg p-3 mb-5 bg-body rounded'>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="name" placeholder="Enter Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter Email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                    <Button variant="danger" type="submit" className='mx-auto d-block'>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
