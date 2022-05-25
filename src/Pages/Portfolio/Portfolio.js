89803c525c3312c3ce9d7a415388a4b8f5ec630f
SohanaN <sohananaz01@gmail.com>
2022-05-26 01:19:12 +0600

    Porfolio added

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
 
 const Portfolio = () => {
     return (
-        <div>
-            <h2>Portfolio</h2>
+        <div className='my-5'>
+            <Container>
+                <Row>
+                    <Col xs={12} lg={6}>
+                        <Card border="danger" className="text-center">
+                            <Card.Header className='text-danger fw-bold' >My Portfolio</Card.Header>
+                            <Card.Body>
+                                <Card.Title>Sohana Nazneen</Card.Title>
+                                <Card.Text>
+                                    Email: sohananaz01@gmail.com
+                                </Card.Text>
+                                <Card.Text>
+                                    Educational Background: B.Sc. (Hons) in CSE
+                                </Card.Text>
+                                <h6 className='text-danger' >Live Links</h6>
+                                <Card.Link href="https://sohanan.github.io/sohana-1st-portfolio/">My portfolio</Card.Link>
+                                <Card.Link href="https://sohanan.github.io/a2-influencer-gear/">Influencer gear</Card.Link>
+                                <Card.Link href="https://halal-grocery.web.app/">Halal Grocery</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={12} lg={6}>
+                        <div className='d-flex justify-content-center' >
+                            <Card style={{ width: '18rem' }} className='text-center'>
+                                <Card.Header className='text-danger fw-bold' >Technologies or Skills</Card.Header>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>Web Design</ListGroup.Item>
+                                    <ListGroup.Item>Web Development</ListGroup.Item>
+                                    <ListGroup.Item>Graphics Design</ListGroup.Item>
+                                    <ListGroup.Item>AutoCad Design</ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
